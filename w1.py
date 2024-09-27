a3ca36d0da1a4c932dc7d2c7578349ef2a608974
Jakub <marciszonekjakub@gmail.com>
2024-09-27 12:45:20 +0300

    w3-begining

--- w1.py
+++ w1.py
@@ -4,13 +4,25 @@ import sys
 import datetime
 import random
 
+##### Def #####
+
+def QuitCheck(inp):
+    if inp.lower() == "quit":
+        sys.exit()
+
 ##### Login Variables #####
 
-print("Hello user, welcome to the Motion Detector! Let's start.")### user input:
+print("Hello user, welcome to the Motion Detector! Let's start.\n\
+type quit whenever you would like to end the session.")### user input:
+
+UserRights = ["Admin", "Super-user", "User"]
 Name = str(input("What's ur name? "))###name and birthday
+QuitCheck(Name)
 LName = str(input("What;s ur lastname? "))
+QuitCheck(LName)
 BDate = input("When you've been born write? \
 The date in (dd/mm/yyyy) format. \n")
+QuitCheck(BDate)
 PDate = datetime.datetime.now()
 BDay, BMonth, BYear = map(str, BDate.split("/"))# map is deviding one 
 # variable into 3 new ones respectivly by .split function where separator
@@ -38,59 +50,49 @@ elif Age >= 18:
 else:
     print(f"Greetings {Name}, you are too young to operate this program. Continue")
 
-while True:
-    print(Name[:2] + LName[:3] + BYear + BMonth + BDay)###username
+print(Name[:2] + LName[:3] + BYear + BMonth + BDay)###username
 
-    ##### Movement detection #####
-    ### test version with random input
-    YesNo = ("yes", "no")
-    Movement = random.choice(YesNo)
-    #Movement = input("There was a movement in the room? (y/n) \n")
-    if (Movement.lower() == "yes" or Movement.lower() == "y"):
-        print("Movement detected")
-    elif (Movement.lower() == "no" or Movement.lower() == "n"):
-        print("Movement not detected")
-    else:
-        print("Invalid data")
+##### Movement detection #####
+### test version with random input
+YesNo = ("yes", "no")
+Movement = random.choice(YesNo)
+#Movement = input("There was a movement in the room? (y/n) \n")
+#QuitCheck(Movement)
+if (Movement.lower() == "yes" or Movement.lower() == "y"):
+    print("Movement detected")
+elif (Movement.lower() == "no" or Movement.lower() == "n"):
+    print("Movement not detected")
+else:
+    print("Invalid data")
 
-    ##### Temperature converter and evaluator #####
+##### Temperature converter and evaluator #####
 
-    print("Celsius to Fahrenheit calculator")
+print("Celsius to Fahrenheit calculator")
 
-    TemD = (input("Choose the units that you want to convert\
-    \n1. Celsius degrees\n2. Fahrenheit degrees\n"))
-    # if TemD == "1":###Celsius to Fahrenheit converter
-    #     CDegrees = float(input("Enter the Celsius degrees: "))
-    #     FDegrees = round((CDegrees * 1.8 + 32), 2)
-    #     print(f"The given temperature {CDegrees}C° is {FDegrees}F°")
-    # elif TemD == "2":
-    #     FDegrees = float(input("Enter the Fahrenheit degrees: "))
-    #     CDegrees = round(((FDegrees - 32) / 1.8), 2)
-    #     print(f"The given temperature {FDegrees}F° is {CDegrees}C°")
-    # else:
-    #     print("Invalid data")
+TemD = (input("Choose the units that you want to convert\
+\n1. Celsius degrees\n2. Fahrenheit degrees\n"))
+QuitCheck(TemD)
+if TemD == "1":###Celsius to Fahrenheit converter
+    CDegrees = float(input("Enter the Celsius degrees: "))
+    QuitCheck(CDegrees)
+    FDegrees = round((CDegrees * 1.8 + 32), 2)
+    print(f"The given temperature {CDegrees}C° is {FDegrees}F°")
+elif TemD == "2":
+    FDegrees = float(input("Enter the Fahrenheit degrees: "))
+    QuitCheck(FDegrees)
+    CDegrees = round(((FDegrees - 32) / 1.8), 2)
+    print(f"The given temperature {FDegrees}F° is {CDegrees}C°")
+else:
+    print("Invalid data")
 
-    CDegrees = random.randint(20, 100)
+# CDegrees = random.randint(20, 100) ###temp tester
 
-    if CDegrees <= 40:
-        print("CPU is dead cold")
-    elif CDegrees > 80:
-        print("[*]RIP CPU [*]")
-    elif CDegrees > 65:
-        print("It's getting hot here ( ͡° ͜ʖ ͡°)")
-    else:
-        print("ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧")
+if CDegrees <= 40:
+    print("CPU is dead cold")
+elif CDegrees > 80:
+    print("[*]RIP CPU [*]")
+elif CDegrees > 65:
+    print("It's getting hot here ( ͡° ͜ʖ ͡°)")
+else:
+    print("ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧")
 
-
-
-    Quit = input("Do you want to quit or restart the programme(quit/restart?) ")
-
-    if Quit.lower() == "quit":
-        print("Thanks for visiting. Welcome back soon.")
-        break
-    elif Quit.lower() == "restart":
-        print("Programme restrting")
-        pass
-    else:
-        print("Invalid command\nshutting down the programme")
-        break
